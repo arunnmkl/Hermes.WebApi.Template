78be4fb20128f133e4a0ca3d283e8cd9c1697bbb
Arunkumar Karunakaran <arun.kumar@dreamorbit.com>
2016-05-26 19:12:47 +0530

    Implemented 1. on authorization 2. refresh token 3. logout method

--- Database/HermesAuthDB/Stored Procedures/dbo.SetTokenExpires.sql
+++ Database/HermesAuthDB/Stored Procedures/dbo.SetTokenExpires.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -6,17 +7,27 @@ GO
 CREATE PROCEDURE [dbo].[SetTokenExpires]
 	-- Add the parameters for the stored procedure here
 	@UserId BIGINT
+	,@UserAuthTokenId VARCHAR(36)
 AS
 BEGIN
+	DECLARE @rowCount INT = 0;
+
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	UPDATE dbo.UserAuthToken
 	SET AccessToken = NULL
 		,ExpiresUtc = GETUTCDATE()
 		,IsLoggedIn = 0
 		,UpdatedDate = GETDATE()
-	WHERE [UserId] = @UserId
+	WHERE [UserId] = @UserId AND UserAuthTokenId = @UserAuthTokenId
 
-	SELECT @@ROWCOUNT
+	SET @rowCount = (
+			SELECT @@ROWCOUNT
+			)
+
+	DELETE
+	FROM dbo.RefreshToken
+	WHERE UserAuthTokenId = @UserAuthTokenId
+
+	SELECT @rowCount
 END
-
 GO
